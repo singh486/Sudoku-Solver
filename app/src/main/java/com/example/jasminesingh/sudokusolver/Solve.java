2cc8acf5575c7ffb2be94376216b909c99de5720
Jasmine <singh486@purdue.edu>
2018-06-26 01:09:34 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/jasminesingh/sudokusolver/Solve.java
@@ -0,0 +1,23 @@
+package com.example.jasminesingh.sudokusolver;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Solve extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Solve");
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.solve, container, false);
+    }
+}
