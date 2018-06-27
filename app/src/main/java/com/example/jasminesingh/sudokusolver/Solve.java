bd0f0653783e22c3489bc42647d4f6c1b92b5bba
Jasmine <singh486@purdue.edu>
2018-06-27 00:04:15 -0700

    Capture image with thumbnail, start work on alertDialog

--- app/src/main/java/com/example/jasminesingh/sudokusolver/Solve.java
+++ app/src/main/java/com/example/jasminesingh/sudokusolver/Solve.java
@@ -1,13 +1,30 @@
 package com.example.jasminesingh.sudokusolver;
 
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Solve extends Fragment {
+    private Button btnCamera;
+    private ImageView capturedImage;
+    private Button btnNext;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -18,6 +35,61 @@ public class Solve extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.solve, container, false);
+        View v = inflater.inflate(R.layout.solve, container, false);
+
+        btnCamera = (Button) v.findViewById(R.id.btnCamera);
+
+        capturedImage= (ImageView) v.findViewById(R.id.capturedImage);
+
+        btnNext = (Button) v.findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog alert = new Dialog(getContext());
+                alert.setContentView(R.layout.alert);
+
+                DisplayMetrics metrics = getResources().getDisplayMetrics();
+                int width = metrics.widthPixels;
+                int height = metrics.heightPixels;
+                alert.getWindow().setLayout((6 * width)/7, (4 * height)/5);
+
+                Button exit = (Button) alert.findViewById(R.id.btnExit);
+                exit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        alert.dismiss();
+                    }
+                });
+
+                alert.show();
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+        return v;
     }
+
+    private void openCamera() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK) {
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+;
+            capturedImage.setImageBitmap(bp);
+        }
+    }
+
+
 }
