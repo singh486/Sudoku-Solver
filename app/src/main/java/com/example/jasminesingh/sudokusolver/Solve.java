c17072091cf03ddec05d71e97115ab573589b1b7
Jasmine <singh486@purdue.edu>
2018-06-29 16:11:45 -0700

    Basic Google Vision functionality

--- app/src/main/java/com/example/jasminesingh/sudokusolver/Solve.java
+++ app/src/main/java/com/example/jasminesingh/sudokusolver/Solve.java
@@ -1,22 +1,36 @@
 package com.example.jasminesingh.sudokusolver;
 
+
 import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.ByteArrayOutputStream;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -24,6 +38,10 @@ public class Solve extends Fragment {
     private Button btnCamera;
     private ImageView capturedImage;
     private Button btnNext;
+    private TextView result;
+    Bitmap bp;
+    String imageText = "";
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -43,22 +61,40 @@ public class Solve extends Fragment {
 
         btnNext = (Button) v.findViewById(R.id.btnNext);
 
+
+
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Dialog alert = new Dialog(getContext());
                 alert.setContentView(R.layout.alert);
 
+                result = (TextView) alert.findViewById(R.id.result);
+
                 DisplayMetrics metrics = getResources().getDisplayMetrics();
                 int width = metrics.widthPixels;
                 int height = metrics.heightPixels;
                 alert.getWindow().setLayout((6 * width)/7, (4 * height)/5);
+
+                if(imageText.equals("")){
+                    result.setText("No text detected");
+                }else{
+                    result.setText(imageText);
+                }
 
                 Button exit = (Button) alert.findViewById(R.id.btnExit);
                 exit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         alert.dismiss();
+                    }
+                });
+
+                Button done = (Button) alert.findViewById(R.id.btnDone);
+                done.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
                     }
                 });
 
@@ -85,11 +121,28 @@ public class Solve extends Fragment {
         // TODO Auto-generated method stub
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK) {
-            Bitmap bp = (Bitmap) data.getExtras().get("data");
-;
+            bp = (Bitmap) data.getExtras().get("data");
+
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(getContext()).build();
+
+            Frame imageFrame = new Frame.Builder()
+
+                    .setBitmap(bp)                 // your image bitmap
+                    .build();
+
+
+            SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
+
+            for (int i = 0; i < textBlocks.size(); i++) {
+                TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
+                imageText = textBlock.getValue();                   // return string
+            }
+
             capturedImage.setImageBitmap(bp);
         }
     }
+
+
 
 
 }
